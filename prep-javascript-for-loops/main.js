fdbc112fd8c4982ae0c1e420767062baa9785f82
Victor Kalinkin <vakalinkin@hotmail.com>
2021-04-29 14:36:55 -0700

    Victor Kalinkin prep-javascript-for-loop (initial submission)

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+for (var a = 0; a < 10; a++) {
+  console.log(a);
+}
+
+console.log('----------');
+
+for (var b = 0; b < 20; b += 2) {
+  console.log(b);
+}
+
+console.log('----------');
+for (var c = 100; c > 0; c--) {
+  console.log('Time till explosion: ' + c);
+}
